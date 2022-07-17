489de0017831e77feee15e3123fdff46682714e8
CEM <109301185+cemvisual@users.noreply.github.com>
2022-07-17 12:10:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-En développement ...
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+//Prefix
+const prefix = "!"
+Client.on("ready", () => {
+    console.log("bot opérationnel")
+      Client.user.setPresence ({
+          activities: [{
+              name: 'CEM COMMUNITY',
+              type: 'LISTENING'
+          }],
+      });
+  });
+
+Client.login("OTg1MjQ0OTk2NzcwODYxMTA2.G0yDDI.ERsd6RSHDtGDg3kzEjgMiSavMu4MMpjtkC5M6Q");
